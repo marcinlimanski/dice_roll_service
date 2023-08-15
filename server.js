be09cc17ec49e5cbd2fef80076a01018aacfc389
Marcin Limanski <marcinlimanski@outlook.com>
2023-08-15 12:20:56 -0400

    src: added random number function

--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const PORT = 3000; // You can change this to any available port
 
 app.use(express.json());
 
-// Sample data
-let dice = [
-    { face: 1, name: '1' },
-    { face: 2, name: '2' },
-];
+function getRandomNumber() {
+    min = 1
+    max = 6
+
+    // Generate a random decimal number between 0 and 1
+    const randomDecimal = Math.random();
+
+    // Scale the random decimal to the desired range
+    const randomNumber = min + randomDecimal * (max - min + 1);
+
+    // Use Math.floor to truncate the decimal part and get an integer within the range
+    return Math.floor(randomNumber);
+}
 
 // GET all items
 app.get('/roll', (req, res) => {
-    res.json(dice);
+    res.json(getRandomNumber());
 });
 
 app.listen(PORT, () => {
